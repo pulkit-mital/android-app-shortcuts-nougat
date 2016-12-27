752a30bf34f4a2dd6e20d4a34da75c3e5d1d28e9
Pulkit Mital <pmital@bitbucket.org>
2016-12-27 23:48:41 +0530

    first commit

---
+++ app/src/main/java/com/pulkit/androidnougatappshortcut/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.pulkit.androidnougatappshortcut;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pulkitmital on 27/12/16.
+ */
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
